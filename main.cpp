b07a4ece8914e33c9e2481d951e30bd896794367
Darius Varnelis <varnelisdarius0@gmail.com>
2026-09-08 13:37:58 +0300

    Add grade mean calculation option.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <iomanip>
 #include <numeric>
+#include<cmath>
 using std::string;
 using std::vector;
 struct studentas {
@@ -10,12 +12,29 @@ struct studentas {
     vector<int> paz;
     int exam;
 };
-void print(studentas& st) {
-    std::cout << std::left << std::setw(17) <<  st.vardas << "|";
-    std::cout << std::left << std::setw(17) << st.pavarde << "|";
+int median(vector<int> paz, int exam) {
+    paz.push_back(exam);
+    sort(paz.begin(), paz.end());
+    if (paz.size() % 2 == 1) {
+        return paz[paz.size()/2];
+    }
+    return (paz[(paz.size()-1)/2]+paz[paz.size()/2])/2;
+}
+void print(studentas& st, int stats) {
+    std::cout << std::left << std::setw(20) <<  st.vardas << "|";
+    std::cout << std::left << std::setw(20) << st.pavarde << "|";
     float suma = std::accumulate(st.paz.begin(), st.paz.end(), 0) + st.exam;
     float vidurkis = suma / (st.paz.size() + 1);
-    std::cout << std::right << std::setw(17) << std::fixed<< std::setprecision(2)<< vidurkis << "|"<<std::endl;
+    float mediana = median(st.paz, st.exam);
+    if (stats == 0) {
+        std::cout << std::right << std::setw(20) << std::fixed<< std::setprecision(2)<< vidurkis << "|"<<std::endl;
+    }
+    else if (stats == 1) {
+        std::cout << std::right << std::setw(20) << std::fixed<< std::setprecision(2)<< mediana << "|"<<std::endl;
+    }
+    else if (stats == 2) {
+        std::cout << std::right << std::setw(20) << std::fixed<< std::setprecision(2)<< vidurkis << "|"<< std::setw(20) << mediana << "|"<<std::endl;
+    }
 }
 
 int main(){
@@ -23,6 +42,7 @@ int main(){
     studentas st;
     int kiekis;
     int studkiekis;
+    int stats;
     std::cout << "Kiek yra studentų? ";
     std::cin >> studkiekis;
     for(int i = 0; i < studkiekis; i++) {
@@ -51,11 +71,22 @@ int main(){
         st.paz.clear();
         st.exam = 0;
     }
-    std::cout << std::left << std::setw(17) <<  "Vardas" << "|";
-    std::cout << std::left << std::setw(17) << "Pavardė" << "|";
-    std::cout << std::left << std::setw(17) << "Galutinis (vid.)" << "|"<<std::endl;
+    std::cout << "Kokių norite duomenų:" << std::endl<< "[0] - vidurkio" << std::endl << "[1] - medianos" << std::endl << "[2] - abiejų"<<std::endl;
+    std::cin >> stats;
+    std::cout << std::left << std::setw(20) <<  "Vardas" << "|";
+    std::cout << std::left << std::setw(20) << "Pavarde" << "|";
+    if (stats == 0) {
+        std::cout << std::left << std::setw(20) << "Galutinis (vid.)" << "|"<<std::endl;
+    }
+    else if (stats == 1) {
+        std::cout << std::left << std::setw(20) << "Galutinis (med.)"<< "|"<<std::endl;
+    }
+    else if (stats == 2) {
+        std::cout << std::left << std::setw(20) <<  "Galutinis (vid.)" << "|"<< std::setw(20) << "Galutinis (med.)"<< "|"<<std::endl;
+    }
+
     for (studentas& stud : grupe) {
-        print(stud);
+        print(stud, stats);
 
     }
 }
